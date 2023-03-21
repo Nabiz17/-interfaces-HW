e77edfeaac1b529ff823f30717b2a1b157cf1312
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-21 19:02:53 +0300

    add implementation of interfaces

---
+++ src/Supermen.java
@@ -0,0 +1,22 @@
+
+public class Supermen implements interfaces.CanFly, interfaces.CanRun, interfaces.CanSwim {
+  @Override
+  public void fly() {
+    System.out.println("Супермен летит");
+  }
+
+  @Override
+  public void run() {
+    System.out.println("Супермен бежит");
+  }
+
+  @Override
+  public void swim() {
+    System.out.println("Супермен плавает");
+  }
+}
+
+
+
+
+
