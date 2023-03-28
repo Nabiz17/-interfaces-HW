6ecc5598fae2c9f4a76a4fefa863a774cf862e6d
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-28 15:17:44 +0300

    add ComparablePerson

---
+++ src/ComparablePerson.java
@@ -0,0 +1,11 @@
+
+import java.util.Comparator;
+public interface ComparablePerson extends Comparator<Person> {
+
+  @Override
+  private static int compare(Person o1, Person o2) {
+    if (!o1.getDateOfBirth().equals(o2.getDateOfBirth())) {
+      return o1.getDateOfBirth().compareTo(o2.getDateOfBirth());
+    } return o1.getDateOfBirth() - o2.getDateOfBirth();
+  }
+}
