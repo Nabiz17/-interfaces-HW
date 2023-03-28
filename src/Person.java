53428382a7824576c7f0642e2e7a63dae34c6d17
Nadezhda Bizina <nadezhda.bizina1433@gmail.com>
2023-03-28 14:58:13 +0300

    add PersonCreator method

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class Person implements CanRun, CanSwim {
+public class Person implements CanRun, CanSwim, ComparablePerson {
 
   //Создайте класс Person, которому при создании передавайте имя и
   // дату рождения в виде строки в формате "ДД.ММ.ГГГГ".
@@ -15,12 +19,30 @@ public class Person implements CanRun, CanSwim {
   // то можно это делать в миллисекундах с использованием getTime()!
 
 private String name;
-private final Date dateOfBirth;
+private final LocalDate dateOfBirth;
 
-  public Person(String name, Date dateOfBirth) {
+  public Person(String name, LocalDate dateOfBirth) {
     this.name = name;
     this.dateOfBirth = dateOfBirth;
   }
+
+  public Person PersonCreator() throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("Введите имя человека: ");
+    String name = br.readLine();
+    System.out.print("Введите дату рождения: ");
+    String dateOfBirthStr = br.readLine();
+
+    String pattern = "DD-mm-yyyy";
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+    LocalDate dateOfBirth = LocalDate.parse(dateOfBirthStr, formatter);
+
+    return new Person(name, dateOfBirth);
+  }
+
+
 
   @Override
   public void run() {
@@ -36,7 +58,7 @@ private final Date dateOfBirth;
     return name;
   }
 
-  public Date getDateOfBirth() {
+  public LocalDate getDateOfBirth() {
     return dateOfBirth;
   }
 
